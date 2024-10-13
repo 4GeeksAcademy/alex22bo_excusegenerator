01d1cabbbfda257db87d6bc9d48b3626806f1df6
Luis Borjas <119633920+alex22bo@users.noreply.github.com>
2024-10-13 19:51:22 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = generador();
+function generador() {
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class ",
+    "when I was sleeping ",
+    "while I was exercising ",
+    "during my lunch ",
+    "while I was praying "
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let al1 = Math.floor(Math.random() * who.length);
+  let al2 = Math.floor(Math.random() * action.length);
+  let al3 = Math.floor(Math.random() * what.length);
+  let al4 = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.querySelector("#excusas").innerHTML =
+    who[al1] + action[al2] + what[al3] + when[al4];
+}
